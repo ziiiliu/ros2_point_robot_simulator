096079cce1ca4484c4f5be4ca0dcf6bc75ea7355
Jan Blumenkamp <jb2270@cam.ac.uk>
2021-03-24 18:26:35 +0000

    Use robomaster control message instead of Twist

--- simulated_robots/simple_robomaster.py
+++ simulated_robots/simple_robomaster.py
@@ -3,7 +3,7 @@ import rclpy
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
-from geometry_msgs.msg import Twist
+from ctrl_msgs.msg import RoboMasterControl
 from .simulated_robot import SimulatedRobotBase
 from .simple_simulator import SimpleSimulator
 
@@ -16,9 +16,9 @@ class RoboMaster(SimulatedRobotBase):
     def __init__(self, uuid, node):
         super().__init__(uuid, node)
 
-        self.velocity = Twist()
+        self.velocity = RoboMasterControl()
         self.velocity_subscription = self.node.create_subscription(
-            Twist, f"/{self.uuid}/cmd_vel", self.velocity_callback, 1
+            RoboMasterControl, f"/{self.uuid}/cmd_vel", self.velocity_callback, 1
         )
 
     def velocity_callback(self, vel):
@@ -26,19 +26,19 @@ class RoboMaster(SimulatedRobotBase):
         self.velocity = vel
 
     def stop(self):
-        self.velocity = Twist()
+        self.velocity = RoboMasterControl()
 
     def step(self, dt):
         self.position += self.orientation.apply(
             np.array(
                 [
                     np.clip(
-                        self.velocity.linear.x,
+                        self.velocity.vx,
                         -self.MAX_V_LINEAR_X_M_S,
                         self.MAX_V_LINEAR_X_M_S,
                     ),
                     -np.clip(
-                        self.velocity.linear.y,
+                        self.velocity.vy,
                         -self.MAX_V_LINEAR_Y_M_S,
                         self.MAX_V_LINEAR_Y_M_S,
                     ),
@@ -54,7 +54,7 @@ class RoboMaster(SimulatedRobotBase):
                     0,
                     0,
                     np.clip(
-                        -self.velocity.angular.z,
+                        -self.velocity.omega,
                         -self.MAX_V_ROT_Z_RAD_S,
                         self.MAX_V_ROT_Z_RAD_S,
                     ),
