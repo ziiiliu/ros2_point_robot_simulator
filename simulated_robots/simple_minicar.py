e4a9d4f36a292a21ed751147ef203b941638459d
Jan Blumenkamp <jb2270@cam.ac.uk>
2021-03-25 15:10:42 +0000

    Minicar sim use minicar control message

--- simulated_robots/simple_minicar.py
+++ simulated_robots/simple_minicar.py
@@ -3,7 +3,7 @@ import rclpy
 import numpy as np
 from scipy.spatial.transform import Rotation as R
 
-from geometry_msgs.msg import Twist
+from ctrl_msgs.msg import MinicarControl
 from .simulated_robot import SimulatedRobotBase
 from .simple_simulator import SimpleSimulator
 
@@ -12,37 +12,24 @@ class Minicar(SimulatedRobotBase):
     def __init__(self, uuid, node):
         super().__init__(uuid, node)
 
-        self.velocity = Twist()
-        self.velocity_subscription = self.node.create_subscription(
-            Twist, f"/{self.uuid}/cmd_vel", self.velocity_callback, 1
+        self.control = MinicarControl()
+        self.control_subscription = self.node.create_subscription(
+            MinicarControl, f"/{self.uuid}/cmd_vel", self.control_callback, 1
         )
 
-    def velocity_callback(self, vel):
+    def control_callback(self, ctrl: MinicarControl):
         self.reset_watchdog()
-        self.velocity = vel
+        self.control = ctrl
 
     def stop(self):
-        self.velocity = Twist()
+        self.control = MinicarControl()
 
     def step(self, dt):
-
-        self.position += self.orientation.apply(
-            np.array(
-                [
-                    np.clip(self.velocity.linear.x, -0.1, 0.1),
-                    0,
-                    0,
-                ]
-            )
-            * dt
-        )
+        v = np.clip(self.control.velocity, -0.2, 0.2)
+        s = np.clip(self.control.steering, -1.0, 1.0) * 0.3
+        self.position += self.orientation.apply(np.array([v, 0, 0]) * dt)
         L = 0.15
-        steering_angle = np.clip(self.velocity.angular.z, -0.3150, 0.3150)
-        self.velocity.linear.x
-        self.orientation *= R.from_euler(
-            "z",
-            np.tan(steering_angle) / L * self.velocity.angular.z * dt,
-        )
+        self.orientation *= R.from_euler("z", np.tan(s) / L * v * dt)
 
 
 def main(args=None):
