50b5428d6d8dee7d2b4befa8835fe4dad1333171
C.J. Lotery <cjl95@cam.ac.uk>
2021-08-20 10:26:08 +0100

    fix bug with accel/vel constraints

--- simulated_robots/simple_fpvquad.py
+++ simulated_robots/simple_fpvquad.py
@@ -12,6 +12,7 @@ from .simple_simulator import SimpleSimulator
 
 from rclpy.qos import qos_profile_sensor_data
 
+
 class FpvQuad(SimulatedRobotBase):
 
     MAX_V_LINEAR_M_S = 10
@@ -30,12 +31,13 @@ class FpvQuad(SimulatedRobotBase):
 
         self.velocity = Twist()
         self.velocity_subscription = self.node.create_subscription(
-            Twist, f"/{self.uuid}/cmd_vel",
+            Twist,
+            f"/{self.uuid}/cmd_vel",
             self.velocity_callback,
             qos_profile=qos_profile_sensor_data,
         )
 
-        self.previous_velocity = np.array([0.0,0.0,0.0])
+        self.previous_velocity = np.array([0.0, 0.0, 0.0])
 
     def velocity_callback(self, vel):
         self.reset_watchdog()
@@ -50,12 +52,14 @@ class FpvQuad(SimulatedRobotBase):
         # much from previous velocity
 
         velocity_array = twist_to_v(self.velocity)
-        previous_velocity_array = self.previous_velocity # self.prev... NOT stored as twist
+        previous_velocity_array = (
+            self.previous_velocity
+        )  # self.prev... NOT stored as twist
 
         # simulate drift if desired
         # velocity_array += self.drift_vel
 
-        # acceleration constraint
+        # acceleration constraint - needs to be absolute magnitude, not axis-wise
         acceleration = (velocity_array - previous_velocity_array) / dt
         acceleration_mag = np.linalg.norm(acceleration)
         if acceleration_mag > self.MAX_A_LINEAR_M_S_S:
@@ -63,7 +67,7 @@ class FpvQuad(SimulatedRobotBase):
             acceleration *= accel_reduction_factor
             velocity_array = previous_velocity_array + acceleration * dt
 
-        # absolute velocity constraint
+        # absolute velocity constraint - same as above
         velocity_mag = np.linalg.norm(velocity_array)
         if velocity_mag > self.MAX_V_LINEAR_M_S:
             vel_reduction_factor = self.MAX_V_LINEAR_M_S / velocity_mag
@@ -91,20 +95,21 @@ class FpvQuad(SimulatedRobotBase):
             * dt,
         )
 
+
 def twist_to_v(vel):
     return np.array([vel.linear.x, vel.linear.y, vel.linear.z])
 
+
 def clip_array(array_1, array_min, array_max):
-    new_array = (
-        np.array(
-            [
-                np.clip(array_1[0], array_min[0], array_max[0]),
-                np.clip(array_1[1], array_min[1], array_max[1]),
-                np.clip(array_1[2], array_min[2], array_max[2])
-            ]
-        )
+    new_array = np.array(
+        [
+            np.clip(array_1[0], array_min[0], array_max[0]),
+            np.clip(array_1[1], array_min[1], array_max[1]),
+            np.clip(array_1[2], array_min[2], array_max[2]),
+        ]
     )
     return new_array
+
 
 def main(args=None):
     rclpy.init(args=args)
